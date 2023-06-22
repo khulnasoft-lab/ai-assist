7aa56035a8c990793a0e03af32351b08420d2e23
AndrasHerczeg <aherczeg@gitlab.com>
2023-06-22 16:27:42 +0200

    Fixed typo

--- codesuggestions/api/v2/api.py
+++ codesuggestions/api/v2/api.py
@@ -3,15 +3,15 @@ from fastapi import APIRouter
 from codesuggestions.api.v2.endpoints import internal, suggestions
 
 
-internal_api_router = APIRouter()
-internal_api_router.prefix = "/v2"
-internal_api_router.include_router(suggestions.router)
-
-
 public_api_router = APIRouter()
-public_api_router.prefix = "/internal/v2"
-public_api_router.include_router(internal.router)
+public_api_router.prefix = "/v2"
+public_api_router.include_router(suggestions.router)
+
+
+internal_api_router = APIRouter()
+internal_api_router.prefix = "/internal/v2"
+internal_api_router.include_router(internal.router)
 
 api_router = APIRouter()
-api_router.include_router(internal_api_router)
 api_router.include_router(public_api_router)
+api_router.include_router(internal_api_router)
