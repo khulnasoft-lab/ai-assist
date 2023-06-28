c724ed400c92c55181be8957481401828a534ec2
AndrasHerczeg <aherczeg@gitlab.com>
2023-06-28 15:07:01 +0200

    Fixed conflicts in api.py

--- codesuggestions/api/v2/api.py
+++ codesuggestions/api/v2/api.py
@@ -4,17 +4,8 @@ from codesuggestions.api.v2.endpoints import internal, suggestions
 from codesuggestions.api.v2.experimental import code
 
 
-public_api_router = APIRouter()
-public_api_router.prefix = "/v2"
-public_api_router.include_router(suggestions.router)
-
-
-internal_api_router = APIRouter()
-internal_api_router.prefix = "/internal/v2"
-internal_api_router.include_router(internal.router)
-
 api_router = APIRouter()
 
 api_router.include_router(suggestions.router, prefix="/v2")
 api_router.include_router(code.router, prefix="/experimental")
-api_router.include_router(internal.router, prefix="internal/v2")
+api_router.include_router(internal.router, prefix="/internal/v2")
