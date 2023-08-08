20b0b27fba7fcb5525716d467c3e404b29f9476b
Bruno Cardoso <bcardoso@gitlab.com>
2023-08-08 16:11:34 +0200

    Log the number of variants during registration

--- codesuggestions/experiments/registry.py
+++ codesuggestions/experiments/registry.py
@@ -23,7 +23,7 @@ class ExperimentRegistry:
         self.experiments = {}
 
     def add_experiment(self, experiment: Experiment):
-        log.info("registering experiment", exp=experiment.name)
+        log.info("registering experiment", exp=experiment.name, variants=len(experiment.variants))
         self.experiments[experiment.name] = experiment
 
     def get_experiment(self, experiment_name: str) -> Optional[Experiment]:
