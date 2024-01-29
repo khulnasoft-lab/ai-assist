787b4e129f594d7edbc0c78fa6fd63860f9af8fe
Sri <srirangan@gmail.com>
2024-01-29 15:12:05 +0100

    Cleanse PII before logging with `sanityze`

--- ai_gateway/api/tasks/compliant_access_logger.py
+++ ai_gateway/api/tasks/compliant_access_logger.py
@@ -1,7 +1,22 @@
 import structlog
+from sanityze.cleanser import *
 
 access_logger = structlog.stdlib.get_logger("api.access")
 
 
 def info(event: str | None = None, *args, **kw):
-    access_logger.info(event, *args, **kw)
+    cleansed_args = cleanse_tuple(args)
+    cleansed_kw = cleanse_dicts(kw)
+    access_logger.info(event, *cleansed_args, **cleansed_kw)
+
+
+def cleanse_tuple(data: tuple) -> tuple:
+    cleanser = Cleanser()
+    dataframe = pd.DataFrame(data)
+    return tuple(cleanser.clean(dataframe).to_dict().values())
+
+
+def cleanse_dicts(data: dict) -> dict:
+    cleanser = Cleanser()
+    dataframe = pd.DataFrame(data)
+    return cleanser.clean(dataframe).to_dict()
