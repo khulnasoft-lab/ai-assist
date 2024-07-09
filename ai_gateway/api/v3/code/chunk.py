125c71fe7c3e6a6b9e55ccc6c14bb228fd6b17f2
maddievn <mvanniekerk@gitlab.com>
2024-07-09 13:11:02 +0200

    Accept a list

--- ai_gateway/api/v3/code/chunk.py
+++ ai_gateway/api/v3/code/chunk.py
@@ -23,16 +23,19 @@ router = APIRouter()
 
 @router.post("/chunk")
 @feature_category(GitLabFeatureCategory.CODE_SUGGESTIONS)
-async def chunk(blob: Blob):
-    language = blob.language
-    content = blob.content
-    line = blob.line
-    # at the moment this just returns a subset of the blob
-    # we want to use tree-sitter to do this instead
-    num_lines = 2
-    lines = content.splitlines()
-    start = max(0, line - (num_lines + 1))
-    end = min(len(lines), line + num_lines)
-    chunk = "\n".join(lines[start:end])
+async def chunk(blobs: list[Blob]):
+    list = []
+    for blob in blobs:
+        language = blob.language
+        content = blob.content
+        line = blob.line
+        # at the moment this just returns a subset of the blob
+        # we want to use tree-sitter to do this instead
+        num_lines = 2
+        lines = content.splitlines()
+        start = max(0, line - (num_lines + 1))
+        end = min(len(lines), line + num_lines)
+        chunk = "\n".join(lines[start:end])
+        list.append(chunk)
 
-    return chunk
+    return list
