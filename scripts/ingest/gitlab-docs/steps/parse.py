4d1b580bb84862f9f64c7cc1b5528022b9418832
Anatoli Babenia <anatoli@rainforce.org>
2024-07-22 19:42:45 +0300

    feat: walk directory tree and split frontmatter

--- scripts/ingest/gitlab-docs/steps/parse.py
+++ scripts/ingest/gitlab-docs/steps/parse.py
@@ -2,6 +2,7 @@
 
 import glob
 import os
+import re
 
 # pylint: disable=direct-environment-variable-reference
 DOC_DIR = os.getenv("GITLAB_DOCS_CLONE_DIR", "")
@@ -9,31 +10,42 @@ ROOT_URL = os.getenv("GITLAB_DOCS_WEB_ROOT_URL", "")
 
 print(f"clone dir: {DOC_DIR}")
 
+FRONT_RE = re.compile(r"---\n(?P<frontmatter>.*?)---\n", re.DOTALL)
 METADATA_KEYS = "title md5sum source source_type source_url".split()
+
+
+def split_md(markdown):
+    """Separates front matter from markdown content and returns pair
+    of strings (content, frontmatter)"""
+    match = FRONT_RE.match(markdown)
+    if not match:
+        return markdown, ""
+    _, end = match.span()
+    return markdown[end:], match.group("frontmatter")
 
 
 def parse(filenames):
     for filename in filenames:
+        # filename: /tmp/gitlab-docs-v17.0.1-ee/doc/user/application_security/dast/checks/798.38.md
+        # source: user/application_security/dast/checks/798.38.md
+        # url: https://docs.gitlab.com/ee/user/application_security/dast/browser/checks/798.38    for filename in filenames:
         source = filename.replace(f"{DOC_DIR}/doc/", "", 1)
+        url = ROOT_URL + "/" + filename.replace('.md', '')
 
-        print(f"parsing: {{ filename: {filename}, source: {source} }}")
+        print(f"parsing: {filename}")
 
-    return []
+        content, front = split_md(open(filename).read())
+
+        print(content[:20])
+        print(front[:10])
+
+    #return parse(filename, source, url)
 
 
 # pylint: disable=pointless-string-statement
 """
-require 'json'
-require_relative "base_content_parser"
-require_relative "docs_content_parser"
-
 def parse(filenames)
   filenames.map do |filename|
-    source = filename.sub("#{DOC_DIR}/doc/", '')
-
-    puts "parsing: { filename: #{filename}, source: #{source} }"
-
-    parser = ::Gitlab::Llm::Embeddings::Utils::BaseContentParser.new( ROOT_URL )
     parser.parse_and_split(
       File.read(filename),
       source
