5bd9e3fcb9c0e106e32a7b2b4239e028255e5f60
Anatoli Babenia <anatoli@rainforce.org>
2024-07-23 05:39:37 +0300

    feat: use dataclass to prepare data for serialization
    Set filename related fields and calculate checksum.
    The checksum field is called .md5sum, but the hash is sha256.

--- scripts/ingest/gitlab-docs/steps/parse.py
+++ scripts/ingest/gitlab-docs/steps/parse.py
@@ -3,6 +3,8 @@
 import glob
 import os
 import re
+from dataclasses import dataclass
+from hashlib import sha256
 
 # pylint: disable=direct-environment-variable-reference
 DOC_DIR = os.getenv("GITLAB_DOCS_CLONE_DIR", "")
@@ -12,6 +14,21 @@ print(f"clone dir: {DOC_DIR}")
 
 FRONT_RE = re.compile(r"---\n(?P<frontmatter>.*?)---\n", re.DOTALL)
 METADATA_KEYS = "title md5sum source source_type source_url".split()
+
+
+@dataclass
+class Metadata:
+    title: str = ""
+    md5sum: str = ""
+    source: str = ""
+    source_type: str = "doc"
+    source_url: str = ""
+
+
+@dataclass
+class RAGChunk:
+    content: str
+    metadata: Metadata
 
 
 def split_md(markdown):
@@ -27,33 +44,25 @@ def split_md(markdown):
 def parse(filenames):
     for filename in filenames:
         # filename: /tmp/gitlab-docs-v17.0.1-ee/doc/user/application_security/dast/checks/798.38.md
-        # source: user/application_security/dast/checks/798.38.md
-        # url: https://docs.gitlab.com/ee/user/application_security/dast/browser/checks/798.38    for filename in filenames:
-        source = filename.replace(f"{DOC_DIR}/doc/", "", 1)
-        url = ROOT_URL + "/" + filename.replace('.md', '')
-
         print(f"parsing: {filename}")
 
-        content, front = split_md(open(filename).read())
+        metadata = Metadata()
+        # source: user/application_security/dast/checks/798.38.md
+        metadata.source = filename.replace(f"{DOC_DIR}/doc/", "", 1)
+        # source_url: https://docs.gitlab.com/ee/user/application_security/dast/browser/checks/798.38
+        metadata.source_url = ROOT_URL + "/" + filename.replace(".md", "")
 
-        print(content[:20])
-        print(front[:10])
+        with open(filename, "r", encoding="utf-8") as file:
+            text = file.read()
+        # TODO: should this field be renamed to .checksum ?
+        metadata.md5sum = sha256(text.encode("utf-8")).hexdigest()
+        content, front = split_md(text)
 
-    #return parse(filename, source, url)
+        yield metadata
 
 
 # pylint: disable=pointless-string-statement
 """
-def parse(filenames)
-  filenames.map do |filename|
-    parser.parse_and_split(
-      File.read(filename),
-      source
-    )
-  end
-end
-
-
 def export(entries)
   log_name = ENV.fetch('GITLAB_DOCS_JSONL_EXPORT_PATH')
   File.delete(log_name) if File.exist?(log_name)
@@ -69,7 +78,8 @@ end
 
 
 def export(entries):
-    return []
+    for entry in entries:
+        print(entry)
 
 
 # pylint: disable=invalid-name
