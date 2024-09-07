3cda4d74b8254ca09f77287884bba12ccfd8ddde
Anatoli Babenia <anatoli@rainforce.org>
2024-09-07 05:33:04 +0300

    fix: only diff unique titles
    Because title is detected once and then copied to each chunk

--- scripts/ingest/gitlab-docs/test_titles.sh
+++ scripts/ingest/gitlab-docs/test_titles.sh
@@ -5,8 +5,8 @@ SECOND=/tmp/python.jsonl
 
 echo "--- Extracting titles grouped under filename"
 
-jq --slurp 'group_by(.metadata.source) | map({source: .[0].metadata.source, titles: [map(.metadata.title)]})' < "$FIRST" > "$FIRST.titles"
+jq --slurp 'group_by(.metadata.source) | map({source: .[0].metadata.source, titles: [map(.metadata.title) | unique]})' < "$FIRST" > "$FIRST.titles"
 
-jq --slurp 'group_by(.metadata.source) | map({source: .[0].metadata.source, titles: [map(.metadata.title)]})' < "$SECOND" > "$SECOND.titles"
+jq --slurp 'group_by(.metadata.source) | map({source: .[0].metadata.source, titles: [map(.metadata.title) | unique]})' < "$SECOND" > "$SECOND.titles"
 
 diff -u3 "$FIRST.titles" "$SECOND.titles"
